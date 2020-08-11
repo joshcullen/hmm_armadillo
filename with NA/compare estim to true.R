006e72f94c8fe8de61aa1a35036e040a66e0d1e2
drvalle1 <drvalle1@users.noreply.github.com>
2020-08-11 12:23:14 -0400

    Commit 3
    Tried to add capability of dealing with NA's but did not work well

---
+++ with NA/compare estim to true.R
@@ -0,0 +1,19 @@
+plot(mod$theta[ngibbs,],type='h')
+
+compare1=function(true,estim){
+  rango=range(c(true,estim))
+  plot(true,estim,xlim=rango,ylim=rango)
+  lines(rango,rango,col='red')
+}
+
+estim.mu.ak=mod$mu.ak[ngibbs,1:4]
+estim.mu.sk=mod$mu.sk[ngibbs,1:4]
+estim.sd.sk=sqrt(mod$sig2.sk[ngibbs,1:4])
+estim.sd.ak=sqrt(mod$sig2.ak[ngibbs,1:4])
+
+ordem=c(2,4,3,1)
+par(mfrow=c(2,2))
+compare1(true=mu.SL.true,estim=estim.mu.sk[ordem])
+compare1(true=mu.TA.true,estim=estim.mu.ak[ordem])
+compare1(true=sd.SL.true,estim=estim.sd.sk[ordem])
+compare1(true=sd.TA.true,estim=estim.sd.ak[ordem])
